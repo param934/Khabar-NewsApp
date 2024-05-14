735d135ab612cc2aa5065a910c9fb4ae29ce4c94
Yogender21505 <yogender21505@iiitd.ac.in>
2024-05-14 10:46:17 +0530

    Final Commit by yogender

--- app/build/intermediates/cxx/Debug/4v1dq2r5/logs/x86_64/configure_command.bat
+++ app/build/intermediates/cxx/Debug/4v1dq2r5/logs/x86_64/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\kumar\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\kumar\\Desktop\\KhabarNews\\app\\src\\main\\cpp" ^
+  "-HC:\\Users\\kumar\\Desktop\\yogender\\KhabarNews\\app\\src\\main\\cpp" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=24" ^
@@ -11,8 +11,8 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\kumar\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\kumar\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\kumar\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\kumar\\Desktop\\KhabarNews\\app\\build\\intermediates\\cxx\\Debug\\4v1dq2r5\\obj\\x86_64" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\kumar\\Desktop\\KhabarNews\\app\\build\\intermediates\\cxx\\Debug\\4v1dq2r5\\obj\\x86_64" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\kumar\\Desktop\\yogender\\KhabarNews\\app\\build\\intermediates\\cxx\\Debug\\4v1dq2r5\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\kumar\\Desktop\\yogender\\KhabarNews\\app\\build\\intermediates\\cxx\\Debug\\4v1dq2r5\\obj\\x86_64" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\kumar\\Desktop\\KhabarNews\\app\\.cxx\\Debug\\4v1dq2r5\\x86_64" ^
+  "-BC:\\Users\\kumar\\Desktop\\yogender\\KhabarNews\\app\\.cxx\\Debug\\4v1dq2r5\\x86_64" ^
   -GNinja
